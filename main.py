99495f5383e01befe51d5c0d0802f5c71616c6e0
BuildTools <leokeviny@gmail.com>
2020-12-21 20:24:06 +0700

    Backend for Javascript Snippet Search Engine

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+# import search
+import numpy as np
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+data = pd.read_csv('data.csv', encoding='ISO-8859-1')
+data = data.dropna()
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/search')
+def search():
+    pass
+
+
+@app.route('/code')
+@app.route('/code/<codeID>')
+def code(codeID=None):
+    if codeID == None:
+        pass
+    else:
+        dataDict = data.loc[int(codeID)].to_dict()
+        return jsonify(dataDict)
